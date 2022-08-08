ef9bf7414dba096344de676d81f6d72d66ba6786
julienerg <julienerg@users.noreply.github.com>
2022-08-08 15:53:29 -0400

    feat(display-iframe): Add example for displaying iframe in modal (#2)

---
+++ display-iframe/src/app/app.functions/crm-card.js
@@ -0,0 +1,21 @@
+exports.main = async (context = {}, sendResponse) => {
+  sendResponse({
+    sections: [
+      {
+        type: 'text',
+        text: 'Clicking the button will open a modal dialog with an iframe that displays the content at the provided URL.'
+      },
+      {
+        type: 'button',
+        text: 'Open iframe',
+        onClick: {
+          type: 'IFRAME',
+          // Width and height of the iframe (in pixels)
+          width: 1000,
+          height: 800,
+          uri: 'https://product.hubspot.com/blog'
+        },
+      },
+    ],
+  });
+};
