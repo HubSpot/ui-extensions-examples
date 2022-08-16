39879c09c86004a2df4904126dae9434342fc958
julienerg <julienerg@users.noreply.github.com>
2022-08-16 11:59:58 -0400

    feat(include-npm-dependencies): Add example for using npm dependencies (#7)

---
+++ include-npm-dependencies/src/app/app.functions/crm-card.js
@@ -0,0 +1,17 @@
+const dayjs = require('dayjs');
+
+exports.main = async (context, sendResponse) => {
+  sendResponse({
+    sections: [
+      {
+        type: 'text',
+        format: 'markdown',
+        text: 'This project demonstrates how to include and use an npm dependency, using [day.js](https://www.npmjs.com/package/dayjs) as an example.'
+      },
+      {
+        type: 'text',
+        text: `Date formatting with day.js: ${dayjs().format('M/D/YYYY')}`
+      }
+    ],
+  });
+};
