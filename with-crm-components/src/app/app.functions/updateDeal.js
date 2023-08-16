e604bdddac1497f8888e8412c5c2031bdf5c3366
tamara-at-hubspot <141843258+tamara-at-hubspot@users.noreply.github.com>
2023-08-16 13:14:46 -0700

    Convert async/await to promise syntax (#26)

--- with-crm-components/src/app/app.functions/updateDeal.js
+++ with-crm-components/src/app/app.functions/updateDeal.js
@@ -1,33 +1,30 @@
 const axios = require('axios');
 
-exports.main = async (context = {}, sendResponse) => {
+exports.main = (context = {}, sendResponse) => {
   const { dealId, dealStage } = context.parameters;
-  const PRIVATE_APP_TOKEN = process.env['PRIVATE_APP_ACCESS_TOKEN'];
-  try {
-    await updateDeal(dealId, dealStage, PRIVATE_APP_TOKEN);
-    sendResponse({ status: 'success' });
-  } catch (e) {
-    sendResponse({ status: 'error', message: e.message });
-  }
+  const token = process.env['PRIVATE_APP_ACCESS_TOKEN'];
+  return updateDeal(token, dealId, dealStage)
+    .then(() => {
+      sendResponse({ status: 'success' });
+    })
+    .catch((e) => {
+      sendResponse({ status: 'error', message: e.message });
+    });
 };
 
-const updateDeal = async (dealId, stage, token) => {
-  try {
-    await axios.patch(
-      `https://api.hubapiqa.com/crm/v3/objects/deals/${dealId}`,
-      {
-        properties: {
-          dealstage: stage,
-        },
+const updateDeal = (token, id, stage) => {
+  return axios.patch(
+    `https://api.hubapiqa.com/crm/v3/objects/deals/${id}`,
+    {
+      properties: {
+        dealstage: stage,
       },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  } catch (error) {
-    throw error;
-  }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
 };
