990e54d406624da64405e4e139abe1c4c1d1657b
atanasiuk-hubspot <57258334+atanasiuk-hubspot@users.noreply.github.com>
2023-11-27 18:19:31 +0300

    Add display-iframe-modal sample (#47)
    * Add display-iframe-modal sample
    
    * Clean up
    
    * Clean up
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add sample to config
    
    * Add cli reference to Readme
    
    * Update DisplayIframe.jsx to have a column layout
    
    * Added GIF
    
    * Update README.md
    
    ---------
    
    Co-authored-by: aanchalHS <110251572+aanchalHS@users.noreply.github.com>

---
+++ display-iframe-modal/src/app/extensions/DisplayIframe.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link, Button, Text, Box, Flex, hubspot } from "@hubspot/ui-extensions";
+
+// Define the extension to be run within the Hubspot CRM
+hubspot.extend((
+  { context, actions } // serverless function is not required for simply displaying an iframe
+) => <Extension context={context} openIframe={actions.openIframeModal} />);
+
+// Define the Extension component, taking in runServerless, context, & openIframe as props
+const Extension = ({ context, runServerless, openIframe }) => {
+  const handleClick = () => {
+    openIframe({
+      uri: "https://wikipedia.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
+      height: 1000,
+      width: 1000,
+    });
+  };
+
+  return (
+    <>
+        <Flex direction="column" align="start" gap="medium">
+        <Text>
+          Clicking the button will open a modal dialog with an iframe that
+          displays the content at the provided URL. Get more info on how to do
+          this here{" "}
+          <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions#open-an-iframe-in-a-modal">
+            here
+          </Link>
+        </Text>
+
+        <Box>
+          <Button type="submit" onClick={handleClick}>
+            Click me
+          </Button>
+        </Box>
+      </Flex>
+    </>
+  );
+};
