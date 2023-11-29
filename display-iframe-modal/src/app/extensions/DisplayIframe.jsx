57fd57f321f473c4b1e08b9f45e5233f5ee5026d
alzheltkovskiy-hubspot <119863330+alzheltkovskiy-hubspot@users.noreply.github.com>
2023-11-29 11:08:21 +0300

    minor fixes (#48)
    * minor fixes
    
    * grammar fixes

--- display-iframe-modal/src/app/extensions/DisplayIframe.jsx
+++ display-iframe-modal/src/app/extensions/DisplayIframe.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
 import { Link, Button, Text, Box, Flex, hubspot } from "@hubspot/ui-extensions";
 
 // Define the extension to be run within the Hubspot CRM
 hubspot.extend((
-  { context, actions } // serverless function is not required for simply displaying an iframe
-) => <Extension context={context} openIframe={actions.openIframeModal} />);
+  { actions } // serverless function is not required for simply displaying an iframe
+) => <Extension openIframe={actions.openIframeModal} />);
 
-// Define the Extension component, taking in runServerless, context, & openIframe as props
-const Extension = ({ context, runServerless, openIframe }) => {
+// Define the Extension component, taking in openIframe as a prop
+const Extension = ({ openIframe }) => {
   const handleClick = () => {
     openIframe({
       uri: "https://wikipedia.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
@@ -18,11 +17,11 @@ const Extension = ({ context, runServerless, openIframe }) => {
 
   return (
     <>
-        <Flex direction="column" align="start" gap="medium">
+      <Flex direction="column" align="start" gap="medium">
         <Text>
           Clicking the button will open a modal dialog with an iframe that
           displays the content at the provided URL. Get more info on how to do
-          this here{" "}
+          this {" "}
           <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions#open-an-iframe-in-a-modal">
             here
           </Link>
