7b6a97569c6d7e06a4dfb760770b69e365fedea8
atanasiuk <atanasiuk@hubspot.com>
2024-02-02 18:47:15 +0700

    Fix adding to cart

--- multi-step-flow/src/app/extensions/components/RestaurantsTable.tsx
+++ multi-step-flow/src/app/extensions/components/RestaurantsTable.tsx
@@ -16,8 +16,10 @@ import {
 import { RestaurantRow } from './RestaurantRow';
 import type { RestaurantsTableProps } from '../types';
 import { MenuItemRow } from './MenuItemRow';
+import { RestaurantMenuPanel } from './RestaurantMenuPanel';
 
 const PAGE_SIZE = 4;
+const MENU_PANEL_ID = 'menu-panel';
 
 export const RestaurantsTable = ({
   searchTerm,
@@ -27,11 +29,8 @@ export const RestaurantsTable = ({
   onPageChange,
 }: RestaurantsTableProps) => {
   const [selectedRestaurant, setSelectedRestaurant] = useState();
-  const [selectedMenuItem, setSelectedMenuItem] = useState();
-  const [bases, setBases] = useState<Array<string>>();
-  const [toppings, setToppings] = useState<Array<string>>();
-  const [premiums, setPremiums] = useState<Array<string>>();
-  const [dressing, setDressing] = useState<string>();
+
+  const handleClosePanel = (reactions) => {};
 
   const pageCount = Math.ceil(restaurants.length / PAGE_SIZE);
   const paginatedRestaurants = restaurants.slice(
@@ -45,170 +44,8 @@ export const RestaurantsTable = ({
     );
   }
 
-  const getBasesError = () => {
-    return !!bases && (bases.length > 2 || bases.length < 1);
-  };
-
-  const getToppingsError = () => {
-    return !!toppings && toppings.length > 4;
-  };
-
-  const getPremiumsError = () => {
-    return !!premiums && premiums.length > 3;
-  };
-
-  const isNotMeetingRequirements = () => {
-    const basesError = !!bases && (bases.length > 2 || bases.length < 1);
-    const toppingsError = !!toppings && toppings.length > 4;
-    const premiumsError = !!premiums && premiums.length > 3;
-    return basesError || toppingsError || premiumsError || !bases || !dressing;
-  };
-
-  const handleAddClick = () => {
-    onAddToCart({
-      restaurantId: selectedRestaurant.id,
-      id: selectedMenuItem.id,
-      name: selectedMenuItem.name,
-      price: selectedMenuItem.price,
-      bases: bases!,
-      toppings: toppings!,
-      premiums: premiums!,
-      dressing: dressing!,
-    });
-  };
-
-  const resetSelections = () => {
-    setSelectedRestaurant(undefined);
-    setSelectedMenuItem(undefined);
-    setBases([]);
-    setToppings([]);
-    setPremiums([]);
-    setDressing(undefined);
-  };
-
-  const makeOption = (option: string) => ({ value: option, label: option });
-
   return (
     <>
-      <Panel
-        id="menu-panel"
-        title={selectedRestaurant ? selectedRestaurant.name : 'Menu Panel'}
-        onClose={() => resetSelections()}
-      >
-        <Flex direction={'column'}>
-          <PanelBody>
-            <PanelSection>
-              {selectedRestaurant && (
-                <Flex direction="column" gap="md">
-                  {selectedMenuItem ? (
-                    <Flex direction="column" gap="sm">
-                      <Heading>{selectedMenuItem.name}</Heading>
-                      <Form>
-                        <ToggleGroup
-                          error={getBasesError()}
-                          name="bases"
-                          label="Bases"
-                          tooltip="Choose 1-2"
-                          required={true}
-                          toggleType="checkboxList"
-                          options={selectedRestaurant.menu.bases.map(
-                            makeOption
-                          )}
-                          onChange={setBases}
-                          value={bases}
-                          validationMessage={
-                            getBasesError()
-                              ? 'Please choose 1 or 2 options'
-                              : ''
-                          }
-                        />
-                        <ToggleGroup
-                          error={getToppingsError()}
-                          name="toppings"
-                          label="Toppings (Optional)"
-                          tooltip="Choose up to 4"
-                          toggleType="checkboxList"
-                          options={selectedRestaurant.menu.toppings.map(
-                            makeOption
-                          )}
-                          onChange={setToppings}
-                          value={toppings}
-                          validationMessage={
-                            getToppingsError()
-                              ? 'Please choose 4 or fewer options'
-                              : ''
-                          }
-                        />
-                        <ToggleGroup
-                          error={getPremiumsError()}
-                          name="premiums"
-                          label="Premiums (Optional)"
-                          tooltip="Choose up to 3"
-                          toggleType="checkboxList"
-                          options={selectedRestaurant.menu.premiums.map(
-                            makeOption
-                          )}
-                          onChange={(items) => setPremiums(items!)}
-                          value={premiums}
-                          validationMessage={
-                            getPremiumsError()
-                              ? 'Please choose 3 or fewer options'
-                              : ''
-                          }
-                        />
-                        <ToggleGroup
-                          name="dressing"
-                          label="Dressing"
-                          required={true}
-                          toggleType="radioButtonList"
-                          options={selectedRestaurant.menu.dressings.map(
-                            makeOption
-                          )}
-                          onChange={setDressing}
-                          value={dressing}
-                        />
-                      </Form>
-                    </Flex>
-                  ) : (
-                    <Table>
-                      <TableBody>
-                        {selectedRestaurant.menu.items.map((item) => (
-                          <MenuItemRow
-                            key={item.id}
-                            item={item}
-                            onClick={() => setSelectedMenuItem(item)}
-                          />
-                        ))}
-                      </TableBody>
-                    </Table>
-                  )}
-                </Flex>
-              )}
-            </PanelSection>
-          </PanelBody>
-          <PanelFooter>
-            <Button
-              onClick={() => setSelectedMenuItem(undefined)}
-              variant="secondary"
-              type="submit"
-              disabled={!selectedMenuItem}
-            >
-              Back
-            </Button>
-            <Button
-              onClick={(event, reactions) => {
-                handleAddClick();
-                reactions.closePanel('menu-panel');
-              }}
-              variant="primary"
-              type="submit"
-              disabled={isNotMeetingRequirements()}
-            >
-              Add
-            </Button>
-          </PanelFooter>
-        </Flex>
-      </Panel>
       <Table
         page={pageNumber}
         paginated={pageCount > 1}
@@ -223,13 +60,26 @@ export const RestaurantsTable = ({
               addToCart={onAddToCart}
               onClick={(reactions) => {
                 setSelectedRestaurant(restaurant);
-                reactions.openPanel('menu-panel');
+                reactions.openPanel(MENU_PANEL_ID);
               }}
               key={restaurant.id}
             />
           ))}
         </TableBody>
       </Table>
+      <Panel
+        id="menu-panel"
+        title={selectedRestaurant ? selectedRestaurant.name : 'Menu Panel'}
+        onClose={() => setSelectedRestaurant(undefined)}
+      >
+        {selectedRestaurant && (
+          <RestaurantMenuPanel
+            restaurant={selectedRestaurant}
+            onAddToCart={onAddToCart}
+            closePanel={(reactions) => reactions.closePanel(MENU_PANEL_ID)}
+          ></RestaurantMenuPanel>
+        )}{' '}
+      </Panel>
     </>
   );
 };
