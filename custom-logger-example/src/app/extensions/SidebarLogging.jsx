bece0ac3613eb98f98327b87d73e53d870df1fc5
Danny Shekhtman <dshekhtman@hubspot.com>
2024-03-13 13:57:12 -0400

    Update copy

--- custom-logger-example/src/app/extensions/SidebarLogging.jsx
+++ custom-logger-example/src/app/extensions/SidebarLogging.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Box,
   Button,
   Divider,
   Flex,
@@ -33,33 +32,34 @@ const SidebarLogging = ({ context }) => {
 
   return (
     <Flex direction="column" align="start" gap="small">
-      <Heading>Test out the logger with the following buttons.</Heading>
-      <Text>Check out the browser's developer console in local dev.</Text>
+      <Heading>Test out the logger with the following buttons</Heading>
+      <Text>
+        The browser's developer console will show your events in local dev.
+      </Text>
+      <Divider />
+      <Heading>Test serverless functions</Heading>
       <Flex gap="small" wrap="wrap">
-        <Button onClick={callServerlessSuccess}>Call serverless success</Button>
-        <Button onClick={callServerlessFail}>Call serverless error</Button>
+        <Button onClick={callServerlessSuccess}>Serverless success ✅</Button>
+        <Button onClick={callServerlessFail}>Serverless error ❌</Button>
       </Flex>
       <Divider />
-      <Box>
-        <Flex direction="column" gap="small">
-          <Heading>Test serverless functions</Heading>
-          <Text>The developer console will show your events in local dev.</Text>
-          <Flex gap="small" wrap="wrap">
-            <Button onClick={() => logger.info('Logging an info!')}>
-              logger.info()
-            </Button>
-            <Button onClick={() => logger.debug('Logging a debug!')}>
-              logger.debug()
-            </Button>
-            <Button onClick={() => logger.warn('Logging a warning!')}>
-              logger.warn()
-            </Button>
-            <Button onClick={() => logger.error('Logging an error!')}>
-              logger.error()
-            </Button>
-          </Flex>
+      <Flex direction="column" gap="small">
+        <Heading>Test different log levels</Heading>
+        <Flex gap="small" wrap="wrap">
+          <Button onClick={() => logger.info('Logging an info!')}>
+            logger.info()
+          </Button>
+          <Button onClick={() => logger.debug('Logging a debug!')}>
+            logger.debug()
+          </Button>
+          <Button onClick={() => logger.warn('Logging a warning!')}>
+            logger.warn()
+          </Button>
+          <Button onClick={() => logger.error('Logging an error!')}>
+            logger.error()
+          </Button>
         </Flex>
-      </Box>
+      </Flex>
       <Divider />
       <Text>
         Deploy the app and crash the card. Use the Trace ID to see what happened
