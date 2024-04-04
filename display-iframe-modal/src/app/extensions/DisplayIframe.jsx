54d88e45ef4ba58e2edef788b4e973f5a44300c0
aanchalHS <110251572+aanchalHS@users.noreply.github.com>
2024-04-04 15:04:25 -0400

    Update DisplayIframe.jsx

--- display-iframe-modal/src/app/extensions/DisplayIframe.jsx
+++ display-iframe-modal/src/app/extensions/DisplayIframe.jsx
@@ -12,6 +12,7 @@ const Extension = ({ openIframe }) => {
       uri: "https://wikipedia.com/", // this is a relative link. Some links will be blocked since they don't allow iframing
       height: 1000,
       width: 1000,
+      title: 'Wikipedia in an Iframe'
     });
   };
 
