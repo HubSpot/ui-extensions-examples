efb39297d2a587663946c2fd341f688c7b2c255d
Bree Hall <bhall@hubspot.com>
2024-04-30 13:33:04 -0400

    Update ActionsExtension.tsx
    - Add example of onError for CrmActionsButton
    - PR Feedback

--- crm-data-components/src/app/extensions/ActionsExtension.tsx
+++ crm-data-components/src/app/extensions/ActionsExtension.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from 'react';
 import {
   Divider,
   Text,
   Flex,
   hubspot,
-  Form,
+  Button,
   Select,
-} from "@hubspot/ui-extensions";
+  ErrorState,
+} from '@hubspot/ui-extensions';
 import {
   CrmPropertyList,
   CrmCardActions,
   CrmActionButton,
-} from "@hubspot/ui-extensions/crm";
+} from '@hubspot/ui-extensions/crm';
 
 // Define the extension to be run within the Hubspot CRM
 hubspot.extend(({ context, runServerlessFunction, actions }) => (
@@ -24,80 +25,91 @@ hubspot.extend(({ context, runServerlessFunction, actions }) => (
 
 const scenariosToPropertiesMap: { [key: string]: string[] } = {
   contact: [
-    "jobtitle",
-    "industry",
-    "date_of_birth",
-    "hs_language",
-    "favorite_color",
+    'jobtitle',
+    'industry',
+    'date_of_birth',
+    'hs_language',
+    'favorite_color',
   ],
   engagement: [
-    "hs_sa_first_engagement_date",
-    "hs_latest_source_data_1",
-    "engagements_last_meeting_booked",
-    "hs_email_sends_since_last_engagement",
-    "hs_time_to_first_engagement",
+    'hs_sa_first_engagement_date',
+    'hs_latest_source_data_1',
+    'engagements_last_meeting_booked',
+    'hs_email_sends_since_last_engagement',
+    'hs_time_to_first_engagement',
   ],
   deals: [
-    "hs_buying_role",
-    "num_associated_deals",
-    "recent_deal_amount",
-    "recent_deal_close_date",
+    'hs_buying_role',
+    'num_associated_deals',
+    'recent_deal_amount',
+    'recent_deal_close_date',
   ],
   nps: [
-    "hs_feedback_show_nps_web_survey",
-    "hs_feedback_last_survey_date",
-    "hs_feedback_last_nps_rating",
-    "hs_feedback_last_nps_follow_up",
+    'hs_feedback_show_nps_web_survey',
+    'hs_feedback_last_survey_date',
+    'hs_feedback_last_nps_rating',
+    'hs_feedback_last_nps_follow_up',
   ],
 };
 
 const options: Array<{ label: string; value: string }> = [
-  { label: "Contact", value: "contact" },
-  { label: "Engagement", value: "engagement" },
-  { label: "Deal Summary", value: "deals" },
-  { label: "NPS", value: "nps" },
+  { label: 'Contact', value: 'contact' },
+  { label: 'Engagement', value: 'engagement' },
+  { label: 'Deal Summary', value: 'deals' },
+  { label: 'NPS', value: 'nps' },
+  { label: 'View Action Button Error', value: 'error' },
 ];
 
 const Extension = ({ context, runServerless, sendAlert }) => {
-  const [scenario, setScenario] = useState<any>("engagement");
+  const [scenario, setScenario] = useState<string>('engagement');
+  const [error, setError] = useState<string[]>();
 
   const currentObjectId = context.crm.objectId;
   const currentObjectTypeId = context.crm.objectTypeId;
 
-  const actionsToPropertiesMap = {
-    contact: {
-      label: "View full record",
-      actionType: "PREVIEW_OBJECT",
-      actionContext: {
-        objectTypeId: currentObjectTypeId,
-        objectId: currentObjectId,
+  const actionsToPropertiesMap = useMemo(() => {
+    return {
+      contact: {
+        label: 'View full record',
+        actionType: 'PREVIEW_OBJECT',
+        actionContext: {
+          objectTypeId: currentObjectTypeId,
+          objectId: currentObjectId,
+        },
       },
-    },
-    engagement: {
-      label: "Schedule an engagement call",
-      actionType: "SCHEDULE_MEETING",
-      actionContext: {
-        objectTypeId: currentObjectTypeId,
-        objectId: currentObjectId,
+      engagement: {
+        label: 'Schedule an engagement call',
+        actionType: 'SCHEDULE_MEETING',
+        actionContext: {
+          objectTypeId: currentObjectTypeId,
+          objectId: currentObjectId,
+        },
       },
-    },
-    deals: {
-      label: "View the latest deal",
-      actionType: "PREVIEW_OBJECT",
-      actionContext: {
-        objectTypeId: "0-3",
-        objectId: 18899287678,
+      deals: {
+        label: 'View the latest deal',
+        actionType: 'PREVIEW_OBJECT',
+        actionContext: {
+          objectTypeId: '0-3',
+          objectId: 18899287678,
+        },
       },
-    },
-    nps: {
-      label: "Email NPS Survey",
-      actionType: "SEND_EMAIL",
-      actionContext: {
-        objectTypeId: currentObjectTypeId,
-        objectId: currentObjectId,
+      nps: {
+        label: 'Email NPS Survey',
+        actionType: 'SEND_EMAIL',
+        actionContext: {
+          objectTypeId: currentObjectTypeId,
+          objectId: currentObjectId,
+        },
       },
-    },
-  };
+      error: {
+        label: 'Action button is disabled',
+        actionType: 'ADD_NOTE',
+        actionContext: {
+          // Missing actionContext will cause an error within CrmActionButton and disable the button
+        },
+      },
+    };
+  }, [currentObjectTypeId, currentObjectId]);
 
   const { label, actionContext, actionType } = actionsToPropertiesMap[scenario];
 
@@ -106,22 +118,22 @@ const Extension = ({ context, runServerless, sendAlert }) => {
       <CrmCardActions
         actionConfigs={[
           {
-            type: "dropdown",
-            label: "More",
+            type: 'dropdown',
+            label: 'More',
             options: [
               {
-                type: "action-library-button",
-                label: "Send email",
-                actionType: "SEND_EMAIL",
+                type: 'action-library-button',
+                label: 'Send email',
+                actionType: 'SEND_EMAIL',
                 actionContext: {
                   objectTypeId: currentObjectTypeId,
                   objectId: currentObjectId,
                 },
               },
               {
-                type: "action-library-button",
-                label: "Add Note",
-                actionType: "ADD_NOTE",
+                type: 'action-library-button',
+                label: 'Add Note',
+                actionType: 'ADD_NOTE',
                 actionContext: {
                   objectTypeId: currentObjectTypeId,
                   objectId: currentObjectId,
@@ -138,21 +150,29 @@ const Extension = ({ context, runServerless, sendAlert }) => {
       </Text>
 
       <Flex direction="row" align="end" gap="small">
-        <Form>
-          <Select
-            label="Property Group"
-            name="property-group"
-            tooltip="Please select a property group to view."
-            value={scenario}
-            onChange={(value) => setScenario(value)}
-            options={options}
-          />
-        </Form>
+        <Select
+          label="Property Group"
+          name="property-group"
+          tooltip="Please select a property group to view."
+          value={scenario}
+          onChange={(value) => {
+            if (value.toString() !== 'error' && error) {
+              setError(undefined);
+            }
+
+            setScenario(value.toString());
+          }}
+          options={options}
+        />
+
         <CrmActionButton
           actionType={actionType}
           actionContext={actionContext}
           variant="primary"
           type="button"
+          onError={(error) => {
+            setError(error);
+          }}
         >
           {label}
         </CrmActionButton>
@@ -160,10 +180,24 @@ const Extension = ({ context, runServerless, sendAlert }) => {
 
       <Divider />
 
-      <CrmPropertyList
-        properties={scenariosToPropertiesMap[scenario]}
-        direction="row"
-      />
+      {!error ? (
+        <CrmPropertyList
+          properties={scenariosToPropertiesMap[scenario]}
+          direction="row"
+        />
+      ) : (
+        <ErrorState title="Trouble fetching properties.">
+          <Text>{error[0]}</Text>
+          <Button
+            onClick={() => {
+              setError(undefined);
+              setScenario('engagement');
+            }}
+          >
+            Reset Error State
+          </Button>
+        </ErrorState>
+      )}
     </>
   );
 };
