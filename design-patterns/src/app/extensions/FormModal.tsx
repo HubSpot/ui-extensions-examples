1885941acb389d35b3257e44ebd75c8760216b1d
Heather Weaver <heather@hweaver.com>
2024-12-18 08:23:35 -0500

    fix: Use ModalFooter in Modal example

--- design-patterns/src/app/extensions/FormModal.tsx
+++ design-patterns/src/app/extensions/FormModal.tsx
@@ -10,6 +10,7 @@ import {
   Link,
   Modal,
   ModalBody,
+  ModalFooter,
   Text,
   TextArea,
   hubspot,
@@ -36,32 +37,33 @@ const FormModal = ({ actions }) => {
               setFormSubmitted(false);
             }}
           >
-            <ModalBody>
-              <Text>
-                When we got all the 0's and 1's lined up, a link will show up in
-                your{' '}
-                <Link href="https://knowledge.hubspot.com/user-management/how-to-set-up-user-notifications-in-hubspot">
-                  Notification Center
-                </Link>
-                . Also, just to be sure, you're gonna get a confirmation email.
-              </Text>
-              <Flex>
-                <Box flex={2}>
-                  {
-                    // Display a success message when the form is submitted.
-                    formSubmitted ? (
-                      <Alert title="Success" variant="success">
-                        Account created successfully.
-                      </Alert>
-                    ) : (
-                      <Form
-                        onSubmit={(e) => {
-                          // Print out the form data when the form is submitted.
-                          console.log(e.targetValue);
-                          setFormSubmitted(true);
-                        }}
-                      >
-                        <Flex direction="column" gap="sm">
+            <Form
+              onSubmit={(e) => {
+                // Print out the form data when the form is submitted.
+                console.log(e.targetValue);
+                setFormSubmitted(true);
+              }}
+            >
+              <ModalBody>
+                <Text>
+                  When we got all the 0's and 1's lined up, a link will show up
+                  in your{' '}
+                  <Link href="https://knowledge.hubspot.com/user-management/how-to-set-up-user-notifications-in-hubspot">
+                    Notification Center
+                  </Link>
+                  . Also, just to be sure, you're gonna get a confirmation
+                  email.
+                </Text>
+                <Flex>
+                  <Box flex={2}>
+                    {
+                      // Display a success message when the form is submitted.
+                      formSubmitted ? (
+                        <Alert title="Success" variant="success">
+                          Account created successfully.
+                        </Alert>
+                      ) : (
+                        <>
                           <Input name="first_name" label="First Name" />
                           <Input name="last_name" label="Last Name" />
                           <Input
@@ -72,35 +74,31 @@ const FormModal = ({ actions }) => {
                             description="We'll never share your email with anyone else."
                             tooltip="Your email is only used for support and account purposes."
                           />
-                          <Flex direction="column" gap="lg">
-                            <TextArea
-                              name="notes"
-                              label="Notes"
-                              required
-                              placeholder="Add any notes about the account here."
-                            />
-                            <ButtonRow>
-                              <Button type="submit" variant="primary">
-                                Create
-                              </Button>
-                              <Button
-                                onClick={() =>
-                                  actions.closeOverlay('modal-form-example')
-                                }
-                              >
-                                Cancel
-                              </Button>
-                            </ButtonRow>
-                          </Flex>
-                        </Flex>
-                      </Form>
-                    )
-                  }
-                </Box>
-                {/* Added an empty Box component to make the form narrower. */}
-                <Box flex={1}>{''}</Box>
-              </Flex>
-            </ModalBody>
+                          <TextArea
+                            name="notes"
+                            label="Notes"
+                            required
+                            placeholder="Add any notes about the account here."
+                          />
+                        </>
+                      )
+                    }
+                  </Box>
+                  {/* Added an empty Box component to make the form narrower. */}
+                  <Box flex={1}>{''}</Box>
+                </Flex>
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" variant="primary">
+                  Create
+                </Button>
+                <Button
+                  onClick={() => actions.closeOverlay('modal-form-example')}
+                >
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Form>
           </Modal>
         }
       >
