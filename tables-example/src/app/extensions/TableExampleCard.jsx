ddabc7c0530c6259cc5e4ef9b0d7edae097564cd
Angela Sun <ansun@hubspot.com>
2025-01-05 01:18:18 +0900

    add table example to card

--- tables-example/src/app/extensions/TableExampleCard.jsx
+++ tables-example/src/app/extensions/TableExampleCard.jsx
@@ -1,66 +1,135 @@
 import React, { useState } from "react";
 import {
-  Divider,
-  Link,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
   Button,
-  Text,
   Input,
   Flex,
+  Heading,
+  Text,
   hubspot,
 } from "@hubspot/ui-extensions";
 
 // Define the extension to be run within the Hubspot CRM
-hubspot.extend(({ context, actions }) => (
-  <TableExampleCard
-    context={context}
-    actions={actions}
-  />
+hubspot.extend(() => (
+  <TableExampleCard />
 ));
 
-// Define the Extension component, taking in runServerless, context, & sendAlert as props
-const TableExampleCard = ({ context, actions }) => {
+const TableExampleCard = () => {
+  const tableData = [
+    {
+      name: "Instapage",
+      publishStatus: "Published",
+      review: "-",
+      installs: "-",
+      lastUpdated: "March 8, 2019 (Amelia Merchant)",
+      createdAt: "March 8, 2019 (Amelia Merchant)",
+    },
+    {
+      name: "Spotify",
+      publishStatus: "Draft",
+      review: "41",
+      installs: "41",
+      lastUpdated: "March 8, 2019 (Amelia Merchant)",
+      createdAt: "March 8, 2019 (Amelia Merchant)",
+    },
+    {
+      name: "Slack",
+      publishStatus: "Published | Draft",
+      review: "136",
+      installs: "136",
+      lastUpdated: "March 8, 2019 (Amelia Merchant)",
+      createdAt: "March 8, 2019 (Amelia Merchant)",
+    },
+    {
+      name: "Survey Monkey",
+      publishStatus: "Published",
+      review: "136",
+      installs: "136",
+      lastUpdated: "March 8, 2019 (Amelia Merchant)",
+      createdAt: "March 8, 2019 (Amelia Merchant)",
+    },
+  ];
+
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(tableData);
+
+// Search for both name and publish status match, case insensitive
+  const handleSearchClick = () => {
+    const foundSearch = tableData.filter(({name, publishStatus}) => {
+      const searchLower = search.toLowerCase();
+      const nameMatch = name.toLowerCase().includes(searchLower);
+      const publishStatusMatch = publishStatus.toLowerCase().includes(searchLower);
+      return nameMatch || publishStatusMatch;
+    });
+
+    setData(foundSearch);
+  };
 
   return (
     <>
-      <Text>
-        <Text format={{ fontWeight: "bold" }}>
-          Your first UI extension is ready!
+      <Flex direction="column" gap="small">
+        <Heading>Table Example</Heading>
+        <Text>
+          Tables can be tricky, especially as they grow in complexity and size.
+          This is an example of how to input, buttons, and scrolling can be
+          implemented.
         </Text>
-        Congratulations, {context.user.firstName}! You just deployed your first
-        HubSpot UI extension. This example demonstrates how you would send
-        parameters from your React frontend to the serverless function and get a
-        response back.
-      </Text>
-      <Flex direction="row" align="end" gap="small">
-        <Input name="text" label="Send" onInput={(t) => setText(t)} />
-        <Button type="submit" onClick={handleClick}>
-          Click me
-        </Button>
+
+        <Flex direction="row"
+        >
+          <Input
+            name="search"
+            placeholder="Search"
+            value={search}
+            onChange={(search) => setSearch(search)}
+          />
+           <Flex
+           justify="end"
+        >
+          <Button
+            size="xs"
+            variant="secondary"
+            type="button"
+            align-self="end"
+            onClick={handleSearchClick}
+          >
+            Button
+          </Button>
+          </Flex>
+
+        </Flex>
+
+        <Table bordered={true} paginated={true} showButtonLabels={true} pageCount="5">
+          <TableHead width="min">
+            <TableRow>
+              <TableHeader width="min">Name</TableHeader>
+              <TableHeader width="min">Publish Status</TableHeader>
+              <TableHeader width="min">Review</TableHeader>
+              <TableHeader width="min">Installs</TableHeader>
+              <TableHeader width="min">Last Updated</TableHeader>
+              <TableHeader width="min">Created At</TableHeader>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {data.map(({name, publishStatus, review, installs, lastUpdated, createdAt}, index) => (
+              <TableRow key={index}>
+                <TableCell width="min">{name}</TableCell>
+                <TableCell width="min">{publishStatus}</TableCell>
+                <TableCell width="min">{review}</TableCell>
+                <TableCell width="min">{installs}</TableCell>
+                <TableCell width="min">{lastUpdated}</TableCell>
+                <TableCell width="min">{createdAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Flex>
-      <Divider />
-      <Text>
-        What now? Explore all available{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extension-components">
-          UI components
-        </Link>
-        , get an overview of{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extensions-overview">
-          UI extensions
-        </Link>
-        , learn how to{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions">
-          add a new custom card
-        </Link>
-        , jump right in with our{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extensions-quickstart">
-          Quickstart Guide
-        </Link>
-        , or check out our{" "}
-        <Link href="https://github.com/HubSpot/ui-extensions-react-examples">
-          code Samples
-        </Link>
-        .
-      </Text>
     </>
   );
 };
