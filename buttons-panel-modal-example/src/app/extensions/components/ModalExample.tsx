c2df9b4fdb76fe6f6e9717a166c62e05328eb4c6
Angela Sun <ansun@hubspot.com>
2025-01-05 12:27:28 +0900

    Create buttons within panels and modal examples

---
+++ buttons-panel-modal-example/src/app/extensions/components/ModalExample.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  ModalBody,
+  ModalFooter,
+  Modal,
+  Heading,
+  Button,
+  Input,
+  Flex,
+  Form,
+  Tag,
+  Text,
+} from '@hubspot/ui-extensions';
+
+export const ModalExample = ({ actions }) => {
+  return (
+    <Modal id="default-modal" title="Dialog Header" width="md">
+      <ModalBody>
+        <Flex direction="column" gap="small">
+          <Text>
+            When we get all the 0's and 1's line up, a link will show in
+            your&nbsp;
+            <Text
+              inline={true}
+              format={{ fontWeight: 'bold', lineDecoration: 'underline' }}
+            >
+              Notification Center
+            </Text>
+            . Also, just to be sure, you're gonna get a confirmation email.
+          </Text>
+          <Flex direction="row" gap="small">
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => actions.closeOverlay('default-modal')}
+            >
+              Create
+            </Button>
+            <Button onClick={() => actions.closeOverlay('default-modal')}>
+              Cancel
+            </Button>
+          </Flex>
+        </Flex>
+      </ModalBody>
+    </Modal>
+  );
+};
---
+++ buttons-panel-modal-example/src/app/extensions/components/PanelExample.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Button,
+  Flex,
+  Panel,
+  PanelSection,
+  PanelBody,
+  PanelFooter,
+  Text,
+} from '@hubspot/ui-extensions';
+
+export const PanelExample = ({ actions }) => {
+  return (
+    <Panel id="default-panel" title="Dialog Header">
+      <PanelBody>
+        <PanelSection>
+          <Text>
+            When we get all the 0's and 1's line up, a link will show in
+            your&nbsp;
+            <Text
+              inline={true}
+              format={{ fontWeight: 'bold', lineDecoration: 'underline' }}
+            >
+              Notification Center
+            </Text>
+            . Also, just to be sure, you're gonna get a confirmation email.
+          </Text>
+        </PanelSection>
+      </PanelBody>
+      <PanelFooter>
+        <Flex direction="row" gap="small">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={() => actions.closeOverlay('default-panel')}
+          >
+            Create
+          </Button>
+          <Button onClick={() => actions.closeOverlay('default-panel')}>
+            Cancel
+          </Button>
+        </Flex>
+      </PanelFooter>
+    </Panel>
+  );
+};
